dd508d425fb3614afd4578b0d55d894954c6ec14
Aleksei Shevchenko <silaxx@gmail.com>
2025-02-05 22:39:57 +0900

    Persistence

--- src/test/java/data/HibernateFullTest.java
+++ src/test/java/data/HibernateFullTest.java
@@ -1,55 +1,40 @@
 package data;
 
 import data.entities.Note;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class HibernateFullTest {
 
-    private SessionFactory sessionFactory;
-
-    @BeforeEach
-    protected void setUp() {
-        // A SessionFactory is set up once for an application!
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure() // configures settings from hibernate.cfg.xml
-                .build();
-        try {
-            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-        }
-        catch (Exception e) {
-            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-            // so destroy it manually.
-            StandardServiceRegistryBuilder.destroy(registry);
-        }
-    }
-
-    @AfterEach
-    protected void tearDown() {
-        if (sessionFactory != null) {
-            sessionFactory.close();
-        }
-    }
-
     @Test
-    public void testBasicUsage() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        List<Note> result = session.createQuery("select u from Note u", Note.class).list();
-        System.out.println("Items: " + result.size());
-        for (Note user : result) {
-            System.out.println("Note (" + user.getHeader() + ") : " + user.getDateCreation() );
+    public void storeLoadMessage() {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("snotes");
+
+        try {
+            EntityManager em = emf.createEntityManager();
+
+            em.getTransaction().begin();
+            List<Note> notes = em.createQuery("select n from Note n", Note.class).getResultList();
+            em.getTransaction().commit();
+
+            assertAll(
+                    () -> assertEquals(1, notes.size()),
+                    () -> assertEquals("Test 1", notes.get(0).getHeader())
+            );
+
+            em.close();
+
+        } finally {
+            emf.close();
         }
-        session.getTransaction().commit();
-        session.close();
     }
 
 }
